b1b02d28de94984b988efec3df3eb90eb1475fde
System-in-a-System <textconstruction@protonmail.com>
2020-06-22 13:05:15 +0300

    Comments update

--- src/StringStack.java
+++ src/StringStack.java
@@ -5,18 +5,18 @@ public interface StringStack {
 	// Current stack size
 	int size(); 
 	
-	// True if stack is empty
+	// true if stack is empty
 	boolean isEmpty(); 	
 	
-	// Add element at the top of stack
+	// Add element at top of stack
 	void push(String element); 	
 	
 	// Return and remove top element,
-	// throw exception if stack is empty
+	// exception if stack is empty
 	String pop(); 		
 	
 	// Return (without removing) top element,
-	// throw exception if stack is empty
+	// exception if stack is empty
 	String peek(); 		
 					
 	
